0289fdca651bed9670a707f8e7d75cbc5b5036a5
Alexey Melezhik <melezhik@gmail.com>
2015-11-02 17:11:20 +0300

    tables-test

---
+++ tables/basic-small-width/story.pl
@@ -0,0 +1,16 @@
+use Test::Stream::Table qw/table/;
+
+my @table = table(
+        max_width => 40,
+        header => [ 'a', 'b', 'c', 'd' ],
+        rows => [
+            [ qw/aaaaaaaaaaaaaaaaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbb ccccccccccccccccccccccc ddddddddddddddddddddddddddddd/ ],
+            [ qw/AAAAAAAAAAAAAAAAAAAAAAAAAA BBBBBBBBBBBBBBBBBBBBB CCCCCCCCCCCCCCCCCCCCCCC DDDDDDDDDDDDDDDDDDDDDDDDDDDDD/ ],
+        ],
+    );
+
+
+print "length: ", length($table[0]), "\n";
+
+print join "\n", @table, "\n";
+
